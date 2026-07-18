5d2d9004353d245aaf0d56616c61160574ea90d5
DragunWF <plarisanmarc2@gmail.com>
2026-07-18 21:40:28 +0800

    chore(config): add cloudinary as one of the allowed hosts for images

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "media.licdn.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
       ...(supabaseHost
         ? [
             {
